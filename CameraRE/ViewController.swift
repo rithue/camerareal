978b8b961dae94123ec144d8f7e00a11cab1f834
eperiyasamy <eperiyasamy@paypal.com>
2019-03-23 10:07:16 -0700

    Initial Commit

---
+++ CameraRE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraRE
+//
+//  Created by Periyasamy, Eswaramoorthy on 3/23/19.
+//  Copyright © 2019 Rithu Eswaramoorthy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
